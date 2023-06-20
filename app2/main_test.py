89d2dbbf0c4b7b62610bcdb3a5244a3d2a1cdfc5
Daniella Omenogor <daniellaomenogor@gmail.com>
2023-06-20 19:05:23 +0000

    change file structure

---
+++ app2/main_test.py
@@ -0,0 +1,19 @@
+from main import return_reversed_string,get_mode
+import os
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    def test_return_reversed_string(self):
+        random_string = "This is a test string"
+        random_reversed_string = "gnirts tset a si sihT"
+        self.assertEqual(random_reversed_string,
+                         return_reversed_string(random_string)
+        )
+
+    def test_get_mode(self):
+        self.assertEqual(os.environ.get("MODE"), get_mode())
+
+
+if __name__ == "__main__":
+    unittest.main()
